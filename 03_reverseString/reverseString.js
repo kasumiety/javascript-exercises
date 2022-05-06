6497749b5e898eb68ed0ef2541c2716789b08830
Sora <nebulis@yandex.ru>
2022-05-06 22:46:14 +0300

    Solved exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    newString = "";
 
+    // Alternative method
+    // for (let i = 0; i < string.length; i++) {
+    //     newString += string.slice(string.length - i  - 1, string.length - i);
+    // }
+
+    for (let i = 0; i < string.length; i++) {
+        newString += string.charAt(string.length - i - 1);
+    }
+
+    return newString;
 };
 
 // Do not edit below this line
