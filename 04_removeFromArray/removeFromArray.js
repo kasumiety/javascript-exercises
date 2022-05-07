a47f0ec66152be97d4d6d8e53224efb2e424c1ed
Sora <nebulis@yandex.ru>
2022-05-07 18:23:12 +0300

    Solved exercise 4 (absolutely devilish)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,35 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+
+    // .filter method
+
+    // let newArray = array.filter(function(element) {
+    //     return element != elementForRemoval;
+    // });
+
+    // return newArray;
+
+    //splice method
+
+    //turns arguments into an array
+
+    args = Array.from(arguments);
+
+    outer: for (let i = 0; i < array.length; i++) {
+
+        for (let j = 0; j < args.length; j++) {
+
+            if (array[i] === args[j]) {
+                array.splice(i, 1);
+                i -= 1;
+                continue outer;
+            }
+
+        }
+
+    }
+
+    return array;
+
 
 };
 
