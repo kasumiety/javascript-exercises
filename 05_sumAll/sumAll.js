e27ff3124ffe98cfe8304e43319cc48391b23c39
Sora <nebulis@yandex.ru>
2022-05-07 19:08:37 +0300

    Solved exercises 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+
+    let sum = 0;
+
+    // catches non-numbers
+    if (typeof startInt != 'number' || typeof endInt != 'number') {
+        return "ERROR";
+    }
+
+    //catches negative values
+    if (startInt < 0 || endInt < 0) {
+        return "ERROR";
+    }
+
+    //makes arguments swap places if the latter is smaller than the former
+    if (endInt < startInt) {
+        [startInt, endInt] = [endInt, startInt];
+    }
+
+    for (let i = startInt; i <= endInt; i++) {
+        sum += i;
+    }
+
+    return sum;
 
 };
 
